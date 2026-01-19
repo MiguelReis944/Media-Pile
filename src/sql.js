5ceee244bf62b5e0bee25c37d409655ce5546f65
Miguel Reis <miguelreisdossantos771@gmail.com>
2026-01-19 15:52:57 -0300

    arrumando banco e ocultando valores do github

--- src/sql.js
+++ src/sql.js
@@ -1,18 +1,21 @@
 import mysql from "mysql2/promise"
+import dotenv from 'dotenv'
+
+dotenv.config({ path: '.env' })
+
 
 export const db = await mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "reis",
-    database: "trabalho",
-    port: 3306,
+    host: process.env.db_host,
+    user: process.env.db_user,
+    password: process.env.db_password,
+    database: process.env.db_database,
+    port: process.env.db_port,
 })
 
-//teste de conexão com o mysql
 try{
     const conn = await db.getConnection()
     console.log("db conectado com sucesso")
     conn.release()
 }catch(err){
-    console.log("deu merda no db:",err)
+    console.log("Não foi possivel conectar no banco de dados:",err)
 }
